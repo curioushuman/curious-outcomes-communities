afd57cd1bf71a31c17dfb79a2d00a92cb9b12f15
Mike Kelly <mike@curioushuman.com.au>
2022-05-18 16:47:06 +1000

    refactor(api): :fire: Removing contacts module until we're actively working on it

--- apps/api/src/modules/contacts/adapter/ports/contact.repository.ts
+++
@@ -1,7 +0,0 @@
-import { TaskEither } from 'fp-ts/lib/TaskEither';
-
-import { Contact } from '../../domain/entities/contact';
-
-export abstract class ContactRepository {
-  abstract findOne(slug: string): TaskEither<Error, Contact>;
-}
