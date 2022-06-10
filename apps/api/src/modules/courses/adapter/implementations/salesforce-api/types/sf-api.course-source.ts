a4abf82128582766acbcdcb35246d6f8d1102d5e
Mike Kelly <mike@curioushuman.com.au>
2022-06-10 17:56:47 +1000

    test(api): :white_check_mark: Adding course id

--- apps/api/src/modules/courses/adapter/implementations/salesforce-api/types/sf-api.course-source.ts
+++ apps/api/src/modules/courses/adapter/implementations/salesforce-api/types/sf-api.course-source.ts
@@ -1,4 +1,5 @@
 import { Array, Null, Optional, Record, Static, String } from 'runtypes';
+import { ExternalId } from '../../../../domain/value-objects/external-id';
 import { SalesforceApiResponseRecord } from './sf-api.response';
 
 /**
@@ -12,9 +13,10 @@ import { SalesforceApiResponseRecord } from './sf-api.response';
  * - Salesforce generally loves to return them as Null
  */
 export const SalesforceApiCourseSource = SalesforceApiResponseRecord.extend({
-  Id: String,
+  Id: ExternalId,
   Summary_quick_year__c: String,
   Slug__c: String,
+  CO_course_ID__c: Optional(String.Or(Null)),
   Date_start__c: Optional(String.Or(Null)),
   Date_end__c: Optional(String.Or(Null)),
 });
@@ -27,6 +29,7 @@ export const salesforceApiCourseSourceFields = [
   'Id',
   'Summary_quick_year__c',
   'Slug__c',
+  'CO_course_ID__c',
   'Date_start__c',
   'Date_end__c',
 ];
@@ -41,4 +44,24 @@ export const SalesforceApiCourseSources = Record({
 
 export type SalesforceApiCourseSources = Static<
   typeof SalesforceApiCourseSources
+>;
+
+/**
+ * We use the below for testing only
+ * Currently we don't support the creation of new records in Salesforce
+ *
+ * NOTE: this is the min. num of fields required to create a record
+ */
+export const SalesforceApiCourseSourceCreate = Record({
+  Subject: String,
+  Status: String,
+  Type: String,
+  Type_sub__c: String,
+  Case_Year__c: String,
+  RecordTypeId: String,
+  CO_course_ID__c: Optional(String),
+});
+
+export type SalesforceApiCourseSourceCreate = Static<
+  typeof SalesforceApiCourseSourceCreate
 >;
--- apps/api/src/modules/courses/adapter/implementations/salesforce-api/types/sf-api.response.ts
+++ apps/api/src/modules/courses/adapter/implementations/salesforce-api/types/sf-api.response.ts
@@ -1,4 +1,4 @@
-import { Record, Static, String, Unknown } from 'runtypes';
+import { Boolean, Record, Static, String, Unknown } from 'runtypes';
 
 export const SalesforceApiResponseAuth = Record({
   access_token: String,
@@ -14,4 +14,19 @@ export const SalesforceApiResponseRecord = Record({
 
 export type SalesforceApiResponseRecord = Static<
   typeof SalesforceApiResponseRecord
+>;
+
+/**
+ * We use the below for testing only
+ *
+ * TODO
+ * - [ ] if we ever properly use this, you'll need to include errors[] in the below
+ */
+export const SalesforceApiResponseCreate = Record({
+  id: String,
+  success: Boolean,
+});
+
+export type SalesforceApiResponseCreate = Static<
+  typeof SalesforceApiResponseCreate
 >;
